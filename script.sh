ce70a42dbf35fabeac2963c89cdb81b81bee8b79
LuaySakr <LuaySakr@gmail.com>
2019-01-10 13:41:28 +0200

    1

--- script.sh
+++ script.sh
@@ -1,2 +1,13 @@
-   'youtube-dl -r 50K -f ' + " 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' " + ' -o "/run/user/1000/gvfs/smb-share:server=personalcloud.local,share=public/arabsciencesDownload/GitHub/Downloads2/' +
-                line[0] + '/%(title)s.%(ext)s"  ' + line[1]) 
+# youtube-dl -r 50M -f  bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best \
+# -o "/run/user/1000/gvfs/smb-share:server=personalcloud.local,share=public/Raspberry/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" \
+#    "https://www.youtube.com/watch?v=6ijb6J5vHLk"
+   
+   youtube-dl -r 50K -f  bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best \
+-o "/run/user/1000/gvfs/smb-share:server=personalcloud.local,share=public/Raspberry/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" \
+   "https://www.youtube.com/user/checkgate"
+   
+
+
+
+
+ 
