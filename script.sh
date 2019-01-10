974778ff1ec2e918efce94c1f7a9a4ca11324295
LuaySakr <luaysakr@gmail.com>
2019-01-10 10:41:28 +0000

    init

---
+++ script.sh
@@ -0,0 +1,2 @@
+   'youtube-dl -r 50K -f ' + " 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' " + ' -o "/run/user/1000/gvfs/smb-share:server=personalcloud.local,share=public/arabsciencesDownload/GitHub/Downloads2/' +
+                line[0] + '/%(title)s.%(ext)s"  ' + line[1]) 
