27bd21efebba03b5c4f51d236981957e1ce5cb0d
LuaySakr <luaysakr@gmail.com>
2019-01-10 14:17:38 +0000

    update for personal cloud 2

--- script.sh
+++ script.sh
@@ -2,12 +2,16 @@
 # -o "/run/user/1000/gvfs/smb-share:server=personalcloud.local,share=public/Raspberry/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" \
 #    "https://www.youtube.com/watch?v=6ijb6J5vHLk"
    
-   youtube-dl -r 50K -f  bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best \
--o "/run/user/1000/gvfs/smb-share:server=personalcloud.local,share=public/Raspberry/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" \
-   "https://www.youtube.com/user/checkgate"
+#    youtube-dl -r 50K -f  bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best \
+# -o "/run/user/1000/gvfs/smb-share:server=personalcloud.local,share=public/Raspberry/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" \
+#    "https://www.youtube.com/user/checkgate"
    
 
 
 
 
  
+    youtube-dl -r 50K -f  bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best \
+-o "/run/user/1000/gvfs/smb-share:server=personalcloud-2.local,share=public/Data/MediaLibrary/Raspberry/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" \
+   "https://www.youtube.com/user/checkgate"
+   
